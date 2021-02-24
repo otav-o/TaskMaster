41300c8346ae658c1910c9b8a6fa6e618494058f
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-24 16:33:57 -0300

    Preenchendo o combobox com dados da tabela Status

--- TaskMasterTutorial/TaskMasterTutorial/Form1.cs
+++ TaskMasterTutorial/TaskMasterTutorial/Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TaskMasterTutorial.Models;
 
 namespace TaskMasterTutorial
 {
     public partial class Form1 : Form
     {
+        private tmDBContext tmContext;
         public Form1()
         {
             InitializeComponent();
+
+            tmContext = new tmDBContext();
+
+            var statuses = tmContext.Statuses.ToList();
+
+            foreach (Status s in statuses)
+                cboStatus.Items.Add(s.ToString()); // ou poderia usar s.Name
         }
     }
 }
--- TaskMasterTutorial/TaskMasterTutorial/Models/Status.cs
+++ TaskMasterTutorial/TaskMasterTutorial/Models/Status.cs
@@ -10,5 +10,10 @@ namespace TaskMasterTutorial.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
     }
 }
