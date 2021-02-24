7458ec1c16b5ff282ca85c344e3b9a50cbc3d51e
otav-o <otaviocacadordioscanio@gmail.com>
2021-02-24 14:35:26 -0300

    Criar a classe Task, adicionar migration sem aquela estar no DbContext (erro proposital)

---
+++ TaskMasterTutorial/TaskMasterTutorial/Migrations/202102241732068_AddTaskToDB.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskMasterTutorial.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTaskToDB : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ TaskMasterTutorial/TaskMasterTutorial/Models/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskMasterTutorial.Models
+{
+    class Task
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime? DueDate { get; set; }
+
+        public int StatusId { get; set; } // seguir a convenção de nomes faz o EF gerar a FK
+
+        public Status Status { get; set; } // navigation properties são diferentes de foreign key. 
+        // Não aparece na tabela do BD
+    }
+}
